2837d14ac32e07b64704c4f5cd560778dbd16097
青紙星 <57782574+qingzhixing@users.noreply.github.com>
2022-03-24 01:46:09 +0800

    Add UserInfo.ts GlobalSettings.ts

---
+++ src/types/GlobalSettings.ts
@@ -0,0 +1,36 @@
+enum GlobalTheme{
+    Dark,
+    Light,
+    FollowTheSystem,
+    GlobalThemeAmount,
+}
+
+//默认设置
+let globalSettings={
+    commonSettings:{
+        //从顶部添加新任务
+        addNewTaskFromTheTop:true,
+        //星标任务置顶
+        tasksWithStarsOnTop:true,
+        //完成任务播放提示音
+        playPromptWhenCompleteTasks:true,
+        //删除前确认
+        confirmBeforeDelete:true,
+    },
+    themeSettings:{
+        globalTheme:GlobalTheme.Light,
+    },
+    smartListSettings:{
+        onImportantList:true,
+        onWithinPlanList:true,
+        onFinishedList:true,
+        onWholeList:true,
+        onAssignedToMeList:true,
+        hideEmptyList:true,
+    },
+    notificationSettings:{
+        onNotificationHint:true,
+    }
+};
+
+export {GlobalTheme,globalSettings};
---
+++ src/types/UserInfo.ts
@@ -0,0 +1,7 @@
+interface UserInfo{
+    username : string;
+    account : string;
+    avatarURI : string;
+}
+
+export default UserInfo;
